e9146ca9d339ea1f5dc498fe392dd709aea9ee58
Matteo <person@place.com>
2016-12-08 01:06:50 -0600

    Added csrf

--- magla/youtube-web/app.py
+++ magla/youtube-web/app.py
@@ -44,7 +44,53 @@ def login_required(test):
 # Controllers.
 #----------------------------------------------------------------------------#
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+	
+@crossdomain(origin='*')
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	#session.clear()
@@ -61,6 +107,7 @@ def home():
 		print video
 		return jsonify(video)
 	return render_template('pages/index.html', db_url=config.db_url, videos=session.get('video'))
+
 
 @app.route('/about')
 def about():
@@ -85,6 +132,7 @@ def get():
 
 
 #need to require a csrf to ensure website endpoint not abused
+@crossdomain(origin='*')
 @app.route('/api', methods=['GET'])
 def api():
 	options = {'video_id': request.args['video_id'], 'played': session['video']}
